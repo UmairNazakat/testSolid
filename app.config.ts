63b0b8fc77304191b16af94310daa7e307773550
unknown <umairnazakat2222@gmail.com>
2025-06-22 09:53:46 -0700

    Update vercel

--- app.config.ts
+++ app.config.ts
@@ -1,12 +1,26 @@
-import { defineConfig } from '@solidjs/start/config';
+// import { defineConfig } from '@solidjs/start/config';
 
+// import tailwindcss from '@tailwindcss/vite';
+
+// export default defineConfig({
+//     server: {
+//         preset: 'cloudflare-pages',
+//     },
+//     vite: {
+//         plugins: [tailwindcss()],
+//     },
+// });
+
+
+
+import { defineConfig } from '@solidjs/start/config';
 import tailwindcss from '@tailwindcss/vite';
+import vercel from 'solid-start-vercel';
 
 export default defineConfig({
-    server: {
-        preset: 'cloudflare-pages',
-    },
-    vite: {
-        plugins: [tailwindcss()],
-    },
+  adapter: vercel(),
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
+
